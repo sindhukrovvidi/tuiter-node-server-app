ff5762705dd385db9d4e54a6945440d2338f5129
Sindhu Krovvidi <sindhukrovvidi@Sindhus-MacBook-Air.local>
2023-06-18 00:05:16 -0400

    added mongo connection

---
+++ tuits/tuits-schema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema(
+  {
+    topic: String,
+    username: String,
+    handle: String,
+    time: String,
+    image: String,
+    title: String,
+    tuit: String,
+    liked: Boolean,
+    likes: Number,
+    replies: Number,
+    retuits: Number,
+  },
+  { collection: "tuits" }
+);
+export default schema;
